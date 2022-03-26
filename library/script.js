db7fcec30f5fe3918e47ba0acd1dbc28055bc808
michaelimmanuel <michael.mimo24@gmail.com>
2022-03-26 14:20:54 +0700

    added delete function

--- library/script.js
+++ library/script.js
@@ -1,10 +1,11 @@
-var library = {};
+var library = [];
 var book_id = 0;
 
 window.addEventListener('DOMContentLoaded', ()=>{
     const overlay = document.querySelector("#overlay");
     const popup_form = document.querySelector("#popup-form");
     const form_submit = document.querySelector('#form-submit');
+    const delete_btn = document.querySelector('#delete');
 
     popup_form.addEventListener('click', ()=>{
         overlay.classList.remove('hidden');
@@ -15,12 +16,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
         overlay.classList.remove('flex');
         overlay.classList.add('hidden');
         get_inputVal();
-    })
-
+    });
+    delete_btn.addEventListener('click', ()=>{
+        delete_book();
+    });
 })
 
 class Book{
-    constructor(name = '', author = '', pages = '0', status = false){
+    constructor(chk = false, name = '', author = '', pages = '0', status = false){
+        this.chk = chk;
         this.name = name;
         this.author = author;
         this.pages = pages;
@@ -42,9 +46,9 @@ function get_inputVal(){
         }
         dict[key] = val;
     });
-    //library.push(new Book(dict.title, dict.author, dict.page, dict.status));
-    library[book_id] = new Book(dict.title, dict.author, dict.page, dict.status);
-    book_id = 0;
+    library.push(new Book(false, dict.title, dict.author, dict.page, dict.status));
+    // library[book_id] = new Book(false, dict.title, dict.author, dict.page, dict.status);
+
     // add_table(dict);
     update_table();
 }
@@ -52,16 +56,25 @@ function get_inputVal(){
 function add_table(val){
     const table = document.querySelector("table");
     let row = document.createElement("tr");
+
     for(const item in val){
         let th = document.createElement("td");
         th.className = "px-6 py-3";
         th.scope = "col";
 
-        if(item == "status"){
+        if(item == "status" || item == "chk"){
             let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
-            checkbox.checked = val[item];
+            
+            if(item === "status"){
             checkbox.className = "text-green-500 w-8 h-8 mr-3 border border-gray-300 rounded"
+            checkbox.checked = val["status"];
+            }
+            else{
+                checkbox.className = "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500";
+                checkbox.id = "checkbox";
+                checkbox.checked = false;
+            }
             th.appendChild(checkbox);
         }
         else{
@@ -83,7 +96,17 @@ function update_table(){
     for(const item in library){
         add_table(library[item]);
     }
+}
 
+function delete_book(){
+    const table = document.querySelector("table");
+    const checkbox = document.querySelectorAll("#checkbox");
+    for(let i = 0; i < checkbox.length; i++){
+        if(checkbox[i].checked){
+            library.pop(i);
+        }
+    }
+    update_table();
 }
 
 
